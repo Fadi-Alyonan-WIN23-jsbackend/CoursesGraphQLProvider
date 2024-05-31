515f0a4dfeb242f4f39a36d02fc514a166944e94
Fadi-Alyonan <fadialyonan@gmail.com>
2024-05-31 16:04:54 +0200

    Update course bug fixet

--- CourseProvider.Infrastructure/Services/CourseService.cs
+++ CourseProvider.Infrastructure/Services/CourseService.cs
@@ -1,4 +1,5 @@
 ﻿using CourseProvider.Infrastructure.Data.Contexts;
+using CourseProvider.Infrastructure.Data.Entities;
 using CourseProvider.Infrastructure.Factories;
 using CourseProvider.Infrastructure.Models;
 using Microsoft.EntityFrameworkCore;
@@ -63,15 +64,57 @@ public class CourseService(IDbContextFactory<DataContext> contextFactory) : ICou
     public async Task<Course> UpdateCourseAsync(CourseUpdateRequest request)
     {
         await using var context = _contextFactory.CreateDbContext();
-        var existingCourse = await context.Courses.FirstOrDefaultAsync(x =>x.Id == request.Id);
+        var existingCourse = await context.Courses
+            .Include(c => c.Prices)
+            .Include(c => c.CourseContent)
+            .ThenInclude(cc => cc.CourseDetails)
+            .FirstOrDefaultAsync(x => x.Id == request.Id);
+
         if (existingCourse == null)
         {
             return null!;
         }
 
         var updatedCourseEntity = CourseFactory.Create(request);
-        updatedCourseEntity.Id = existingCourse.Id;
+
         context.Entry(existingCourse).CurrentValues.SetValues(updatedCourseEntity);
+
+        if (updatedCourseEntity.Prices != null)
+        {
+            if (existingCourse.Prices == null)
+            {
+                existingCourse.Prices = new CoursePriceEntity();
+            }
+            context.Entry(existingCourse.Prices).CurrentValues.SetValues(updatedCourseEntity.Prices);
+        }
+        else
+        {
+            existingCourse.Prices = null;
+        }
+
+        if (updatedCourseEntity.CourseContent != null)
+        {
+            if (existingCourse.CourseContent == null)
+            {
+                existingCourse.CourseContent = new CourseContentEntity();
+            }
+            context.Entry(existingCourse.CourseContent).CurrentValues.SetValues(updatedCourseEntity.CourseContent);
+
+            existingCourse.CourseContent.CourseDetails.Clear();
+            foreach (var detail in updatedCourseEntity.CourseContent.CourseDetails)
+            {
+                existingCourse.CourseContent.CourseDetails.Add(new CourseDetailsItemEntity
+                {
+                    Id = detail.Id,
+                    Title = detail.Title,
+                    Description = detail.Description
+                });
+            }
+        }
+        else
+        {
+            existingCourse.CourseContent = null;
+        }
 
         await context.SaveChangesAsync();
         return CourseFactory.Create(existingCourse);
